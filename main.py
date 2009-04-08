b9eeb83a3606ba4256f6649ef7cfb2d849f3071c
Desintegr <desintegr@gmail.com>
2009-04-08 17:17:32 +0200

    message open your web browser

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ PORT = 8080
 
 if __name__ == '__main__':
   server = Server((HOSTNAME, PORT), Handler)
+  print "server running - open your web browser to http://%s:%s" % (HOSTNAME, PORT)
 
   try:
     server.serve_forever()
